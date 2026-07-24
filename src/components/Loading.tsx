d0d1e574b61f8b81838ee3c60148f6679992bcc4
Ahmad <4hmadraza021@gmail.com>
2026-07-24 19:19:16 +0500

    Updated portfolio

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -63,8 +63,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span>&nbsp; AI Engineer &nbsp;</span> <span>&nbsp; Full Stack Developer &nbsp;</span>
-            <span>&nbsp; AI Engineer &nbsp;</span> <span>&nbsp; Full Stack Developer &nbsp;</span>
+            <span>&nbsp; AI Engineer &nbsp;</span> <span>&nbsp; Backend Engineer &nbsp;</span>
+            <span>&nbsp; AI Engineer &nbsp;</span> <span>&nbsp; Backend Engineer &nbsp;</span>
           </Marquee>
         </div>
         <div
